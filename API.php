a36927d194bd6ec1de05ea81cd4aee6b75e489d4
Ismael Henrique <76009905+ismael211@users.noreply.github.com>
2022-03-24 08:39:22 -0300

    Update API.php

--- API.php
+++ API.php
@@ -1,15 +1,28 @@
 <?php
 
 $year = date('Y');
-
+//URL da API
 $url = 'https://brasilapi.com.br/api/feriados/v1/'.$year;
+
+$datahj = date('Y/m/d');
+//$datahj = date('2022-12-25');
+echo date('Y-m-d', strtotime($datahj)); //Formatando a data atual
 
 echo "<pre>";
 
 $feriados = json_decode(file_get_contents($url)); //versão resumida
 
-
+$cont = 0;
 foreach ($feriados as $a) {
-    echo $a->type.'<br>';   
+    if($datahj == $a->date){
+        $cont++;
+    }
+    //echo $a->date.'<br>';   
+}
+if ($cont > 0){
+    echo "\nHoje é um feriado!";
+
+}else{
+    echo "\nHoje não é um feriado!";
 }
 
